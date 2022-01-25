b727b7a7b1ac6d4b46db3d76e01a9d5f5553180e
echoeyecodes <femiobajuluwa@gmail.com>
2022-01-25 15:34:02 +0100

    removed migrated/unused functions from welcome activity

--- app/src/main/java/com/echoeyecodes/clipclip/activities/WelcomeActivity.kt
+++ app/src/main/java/com/echoeyecodes/clipclip/activities/WelcomeActivity.kt
@@ -1,32 +1,22 @@
 package com.echoeyecodes.clipclip.activities
 
 import android.Manifest
-import android.content.ContentValues
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.arthenica.ffmpegkit.*
 import com.echoeyecodes.clipclip.databinding.ActivityWelcomeBinding
 import com.echoeyecodes.clipclip.utils.AndroidUtilities
 import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
-import android.os.Environment
-import android.provider.MediaStore
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.net.toUri
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.echoeyecodes.clipclip.R
 import com.echoeyecodes.clipclip.adapters.VideoAdapter
 import com.echoeyecodes.clipclip.callbacks.VideoAdapterCallback
 import com.echoeyecodes.clipclip.model.VideoModel
 import com.echoeyecodes.clipclip.utils.ActivityUtil
 import com.echoeyecodes.clipclip.utils.CustomItemDecoration
 import com.echoeyecodes.clipclip.viewmodels.VideoSelectionActivityViewModel
-import java.io.File
 
 
 class WelcomeActivity : AppCompatActivity(), VideoAdapterCallback {
@@ -39,8 +29,6 @@ class WelcomeActivity : AppCompatActivity(), VideoAdapterCallback {
     private val viewModel by lazy { ViewModelProvider(this)[VideoSelectionActivityViewModel::class.java] }
 
     companion object {
-        const val CREATE_FILE = 1
-        const val ACCESS_FOLDER = 2
         const val PERMISSIONS_REQUEST_CODE = 3
     }
 
@@ -60,7 +48,6 @@ class WelcomeActivity : AppCompatActivity(), VideoAdapterCallback {
         viewModel.getVideosLiveData().observe(this, {
             adapter.submitList(it)
         })
-//        executeVideoEdit()
         checkDataAccessPermissions()
     }
 
@@ -81,75 +68,10 @@ class WelcomeActivity : AppCompatActivity(), VideoAdapterCallback {
         }
     }
 
-    private fun executeVideoEdit(path: Uri) {
-        val iUri = FFmpegKitConfig.getSafParameterForRead(this, Uri.parse("selectedUrl!!"))
-        val oUri = if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
-            FFmpegKitConfig.getSafParameterForWrite(this, path)
-        } else {
-            path
-        }
-        AndroidUtilities.log(oUri.toString())
-
-        try {
-            FFmpegKit.executeAsync(
-                "-i $iUri -c:v mpeg4 $oUri",
-                { session ->
-                    AndroidUtilities.log("FFMPEG process exited with state ${session.state} and return code ${session.returnCode}")
-                    val returnCode = session.returnCode
-                    if (ReturnCode.isSuccess(returnCode)) {
-                        AndroidUtilities.log("Success")
-                        //operation successfull
-                    } else {
-                        AndroidUtilities.log("Failed")
-                        //operation failed
-                    }
-                }, {
-//                AndroidUtilities.log(it.message.toString())
-                }) {
-                //executes repeatedly
-//            val progress = (time / videoduration) * 100
-                AndroidUtilities.log(it.time.toString())
-            }
-        } catch (exception: Exception) {
-            AndroidUtilities.log("An unknown exception occurred")
-        }
-    }
-
     override fun onVideoSelected(model: VideoModel) {
         val selectedUrl = model.getVideoUri()
         AndroidUtilities.log(selectedUrl.toString())
         ActivityUtil.startVideoActivity(this, selectedUrl.toString(), model.duration)
-    }
-
-    private fun createFile(filename: String): Uri? {
-        val contentValues = ContentValues()
-        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "")
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            contentValues.put(
-                MediaStore.MediaColumns.RELATIVE_PATH,
-                Environment.DIRECTORY_DCIM.plus("/").plus(getString(R.string.app_name))
-            )
-            contentValues.put(
-                MediaStore.MediaColumns.DISPLAY_NAME,
-                filename.plus("_${System.currentTimeMillis()}")
-            )
-            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
-            contentResolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, contentValues)
-        } else {
-            createFileLegacy(filename)
-        }
-    }
-
-    private fun createFileLegacy(filename: String): Uri {
-        val file = File(Environment.getExternalStorageDirectory(), getString(R.string.app_name))
-        if (!file.exists()) {
-            file.mkdir()
-        }
-        return File(file, filename.plus(".mp4")).apply {
-            if (exists()) {
-                delete()
-            }
-        }.absolutePath.toUri()
     }
 
     override fun onRequestPermissionsResult(
@@ -161,16 +83,6 @@ class WelcomeActivity : AppCompatActivity(), VideoAdapterCallback {
         val granted = grantResults.all { it == PackageManager.PERMISSION_GRANTED }
         if (granted) {
             viewModel.fetchVideosFromFileSystem()
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, result: Intent?) {
-        super.onActivityResult(requestCode, resultCode, result)
-
-        if (requestCode == CREATE_FILE && resultCode == RESULT_OK) {
-            if (result != null) {
-                executeVideoEdit(result.data!!)
-            }
         }
     }
 
