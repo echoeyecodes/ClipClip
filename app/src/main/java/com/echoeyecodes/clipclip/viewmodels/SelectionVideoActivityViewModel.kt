82dee172776a4f73fda3d985561a240371dd0c18
echoeyecodes <femiobajuluwa@gmail.com>
2022-01-29 11:55:34 +0100

    moved content resolver to IO thread

--- app/src/main/java/com/echoeyecodes/clipclip/viewmodels/SelectionVideoActivityViewModel.kt
+++ app/src/main/java/com/echoeyecodes/clipclip/viewmodels/SelectionVideoActivityViewModel.kt
@@ -7,6 +7,9 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.echoeyecodes.clipclip.models.VideoModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class SelectionVideoActivityViewModel(application: Application) : AndroidViewModel(application) {
     private val videoData = MutableLiveData<List<VideoModel>>(ArrayList())
@@ -18,37 +21,39 @@ class SelectionVideoActivityViewModel(application: Application) : AndroidViewMod
 
     @SuppressLint("Range")
     fun fetchVideosFromFileSystem() {
-        val contentResolver = getApplication<Application>().applicationContext.contentResolver
-        val uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+        CoroutineScope(Dispatchers.IO).launch {
+            val contentResolver = getApplication<Application>().applicationContext.contentResolver
+            val uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
 
-        val projection = arrayOf(
-            MediaStore.Video.Media._ID,
-            MediaStore.Video.Media.BUCKET_DISPLAY_NAME,
-            MediaStore.Video.Media.TITLE,
-            MediaStore.Video.Media.DATA,
-            MediaStore.Video.Media.DURATION
-        )
+            val projection = arrayOf(
+                MediaStore.Video.Media._ID,
+                MediaStore.Video.Media.BUCKET_DISPLAY_NAME,
+                MediaStore.Video.Media.TITLE,
+                MediaStore.Video.Media.DATA,
+                MediaStore.Video.Media.DURATION
+            )
 
-        val cursor = contentResolver.query(
-            uri,
-            projection,
-            null,
-            null,
-            MediaStore.MediaColumns.DATE_ADDED + " DESC"
-        )
+            val cursor = contentResolver.query(
+                uri,
+                projection,
+                null,
+                null,
+                MediaStore.MediaColumns.DATE_ADDED + " DESC"
+            )
 
-        if (cursor != null) {
-            val _videoList = ArrayList<VideoModel>()
-            while (cursor.moveToNext()) {
-                val id = cursor.getLong(cursor.getColumnIndex(projection[0]))
-                val title = cursor.getString(cursor.getColumnIndex(projection[2]))
-                val duration = cursor.getLong(cursor.getColumnIndex(projection[4]))
-                val path = cursor.getString(cursor.getColumnIndex(projection[3]))
+            if (cursor != null) {
+                val _videoList = ArrayList<VideoModel>()
+                while (cursor.moveToNext()) {
+                    val id = cursor.getLong(cursor.getColumnIndex(projection[0]))
+                    val title = cursor.getString(cursor.getColumnIndex(projection[2]))
+                    val duration = cursor.getLong(cursor.getColumnIndex(projection[4]))
+                    val path = cursor.getString(cursor.getColumnIndex(projection[3]))
 
-                _videoList.add(VideoModel(id, title, path, duration))
+                    _videoList.add(VideoModel(id, title, path, duration))
+                }
+                cursor.close()
+                videoData.postValue(_videoList)
             }
-            cursor.close()
-            videoData.value = _videoList
         }
     }
 }
